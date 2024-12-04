461e8b36d96184493c2ac7ad9f1d4179299aa1bf
Hector Perez <hector.perez.m@hotmail.com>
2024-12-03 21:55:46 -0300

    day 3 2024 part 2

--- 2024/3.rb
+++ 2024/3.rb
@@ -1,10 +1,16 @@
 
 sum = 0
 File.open("3.txt", "r") do |f|
+  do_sum = true
   f.each_line do |line|
-    line.scan(/mul\(\d+,\d+\)/) do |match|
-      mult = match.gsub(/mul\((\d+,\d+)\)/, '\1').split(",").map(&:to_i)
-      sum += mult.inject(:*)
+    line.scan(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/) do |match|
+      do_sum = true if match.first == "do()"
+      do_sum = false if match.first == "don't()"
+
+      next if match.first == "don't()" || match.first == "do()" 
+
+      mult = match.first.gsub(/mul\((\d+,\d+)\)/, '\1').split(",").map(&:to_i)
+      sum += mult.inject(:*) if do_sum
     end
   end
 end
